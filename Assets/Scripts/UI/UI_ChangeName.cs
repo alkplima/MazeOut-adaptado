48ef19b7770acdbdae6cac64b6869a950223e2d7
alkplima <alkplima@gmail.com>
2023-11-22 00:20:43 -0300

    new: add nomePaciente em PlayerPrefs para salvar entre sessões

--- Assets/Scripts/UI/UI_ChangeName.cs
+++ Assets/Scripts/UI/UI_ChangeName.cs
@@ -1,11 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_ChangeName : MonoBehaviour
 {
+    public InputField nomeInputField;
+
+    public void OnEnable()
+    {
+        if (PlayerPrefs.HasKey("NomePaciente"))
+        {
+            VariaveisGlobais.nomePaciente = PlayerPrefs.GetString("NomePaciente");
+            AtualizarValorInputField();
+        }
+
+        Debug.Log("NomePaciente: " + VariaveisGlobais.nomePaciente);
+    }
     public void ChangeName(string name)
     {
         VariaveisGlobais.nomePaciente = name;
+        PlayerPrefs.SetString("NomePaciente", name);
+    }
+
+    private void AtualizarValorInputField()
+    {
+        if (nomeInputField != null)
+        {
+            nomeInputField.text = VariaveisGlobais.nomePaciente;
+        }
     }
 }
